88cefb4a08bb2903ffae9ae9ea86bdd43ada6a31
Hollow-YK <normal.wzj@qq.com>
2026-05-30 00:44:13 +0800

    style: 主页修改

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,8 +1,10 @@
 import { defineClientConfig } from 'vuepress/client'
 import './styles/custom.css'
+import HTMLBlock from './components/HTMLBlock.vue'
 
 export default defineClientConfig({
   enhance({ app }) {
+    app.component('HTMLBlock', HTMLBlock)
     import('./components/Redirect.vue').then((module) => {
       app.component('Redirect', module.default)
     })
